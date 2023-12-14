754858952a27a0c86811fa426c54d91755237570
venkatsundaraj <venkateshsundarasan@gmail.com>
2023-12-14 11:50:13 +0530

    throwNumber-script

--- script.js
+++ script.js
@@ -18,7 +18,14 @@ const multiplyNumber = function (a, b) {
   console.log(a, b)
   return a * b
 }
+
+const thrownNumber = function (a, b) {
+  console.log(a, b)
+  return a % b
+}
+
 addNumber(1, 3)
 divideNumber(4, 2)
 subractNumber(2, 4)
 multiplyNumber(2, 6)
+thrownNumber(2, 8)
