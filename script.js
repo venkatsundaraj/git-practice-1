e8a4f8c80379c0083e06cfe66c15ff0a958567a9
venkatsundaraj <venkateshsundarasan@gmail.com>
2023-12-14 12:55:30 +0530

    fraction-script

--- script.js
+++ script.js
@@ -27,9 +27,15 @@ const thrownNumber = function (a, b) {
   return a % b
 }
 
+const giveFraction = function (a, b) {
+  console.log(a, b)
+  return a / b
+}
+
 addNumber(1, 3)
 divideNumber(4, 2)
 subractNumber(2, 4)
 multiplyNumber(2, 6)
 constructString(2, 4)
 thrownNumber(2, 8)
+giveFraction(3, 4)
