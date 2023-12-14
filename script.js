ef92eae029046a4253d8c04e2a3094e416b99bb4
venkatsundaraj <venkateshsundarasan@gmail.com>
2023-12-14 13:14:53 +0530

    addnumber-main

--- script.js
+++ script.js
@@ -2,29 +2,5 @@ const addNumber = function (a, b) {
   console.log(a, b)
   return a + b
 }
+
 addNumber(1, 3)
-
-const subractNumber = function (a, b) {
-  console.log(a, b)
-  return a - b
-}
-
-const divideNumber = function (a, b) {
-  console.log(a, b)
-  return a / b
-}
-
-const multiplyNumber = function (a, b) {
-  console.log(a, b)
-  return a * b
-}
-
-const constructString = function (a, b) {
-  console.log(a, b)
-  return `${a} by ${b}`
-}
-addNumber(1, 3)
-divideNumber(4, 2)
-subractNumber(2, 4)
-multiplyNumber(2, 6)
-constructString(2,4)
