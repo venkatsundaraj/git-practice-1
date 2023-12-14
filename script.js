777ba1e10c3f353b60a109decfdd84217ec8b8b5
venkatsundaraj <112150254+venkatsundaraj@users.noreply.github.com>
2023-12-14 11:33:12 +0530

    division main

--- script.js
+++ script.js
@@ -8,5 +8,11 @@ const subractNumber = function (a, b) {
   console.log(a, b)
   return a - b
 }
+
+const divideNumber = function (a, b) {
+  console.log(a, b)
+  return a/b
+}
 addNumber(1, 3)
 subractNumber(2, 4)
+divideNumber(4, 2)
