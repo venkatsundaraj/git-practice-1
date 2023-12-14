78a8f5b92c305f1f2040a39a75de6824476b20c6
venkatsundaraj <venkateshsundarasan@gmail.com>
2023-12-14 11:35:34 +0530

    multiply-script

--- script.js
+++ script.js
@@ -8,5 +8,11 @@ const subractNumber = function (a, b) {
   console.log(a, b)
   return a - b
 }
+
+const multiplyNumber = function (a, b) {
+  console.log(a, b)
+  return a * b
+}
 addNumber(1, 3)
 subractNumber(2, 4)
+multiplyNumber(2, 6)
