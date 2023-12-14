cc6501910828351f25d7c305d1a993c9cf8e3521
venkatsundaraj <112150254+venkatsundaraj@users.noreply.github.com>
2023-12-14 12:30:32 +0530

    construct-string-main

--- script.js
+++ script.js
@@ -18,7 +18,13 @@ const multiplyNumber = function (a, b) {
   console.log(a, b)
   return a * b
 }
+
+const constructString = function (a, b) {
+  console.log(a, b)
+  return `${a} by ${b}`
+}
 addNumber(1, 3)
 divideNumber(4, 2)
 subractNumber(2, 4)
 multiplyNumber(2, 6)
+constructString(2,4)
